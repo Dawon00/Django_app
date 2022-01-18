582ad66dd0724bca2d3963e37d82b9bf15862cf7
Dawon00 <da6921@naver.com>
2022-01-18 23:55:04 +0900

    add homepage

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from mysite.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
 ]
